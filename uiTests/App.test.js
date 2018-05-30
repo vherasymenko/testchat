0f1592032360829d82018ddc26255c1806e5b1e8
Volodymyr Herasymenko <vherasymenko@Admins-MBP.localdomain>
2018-05-30 14:09:37 +0300

    Add Puppeteer

---
+++ uiTests/App.test.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+const URL = 'https://www.google.com';
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch({headless: false});
+    page = await browser.newPage();
+    await page.goto(URL);    
+    await page.setViewport({width, height});
+});
+
+afterAll(() => {
+    browser.close();
+})
+
+describe("Check opening google page", () => {
+    test('Check title of the page: ', async() => {
+        const html = await page.$eval('title', e => e.innerHTML);
+
+        expect(html).toBe('Google');
+    })
+})
+
+test('Check searching results:', async() => {
+    const searchQuery = 'TechMagic';
+    const title = 'TechMagic - Пошук Google';
+
+    await page.type('#lst-ib', searchQuery);
+    await page.keyboard.press('Enter');
+
+    await page.waitForSelector('div.g');
+    const pageTitleSelector = await page.$eval('title', e => e.innerHTML);
+    expect(pageTitleSelector).toBe(title);
+
+    const gResult = await page.$$('div.g');
+    expect(gResult.length).toBe(11);
+
+})
